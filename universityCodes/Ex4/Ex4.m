9f65a6c5f85b7872c097fc0ba1d5b4b76d1ee25f
Roxana <93798851+Roxa-na@users.noreply.github.com>
2022-10-21 00:23:21 +0200

    new updtates

--- universityCodes/Ex4/Ex4.m
+++ universityCodes/Ex4/Ex4.m
@@ -5,6 +5,9 @@ clc
 clear
 close
 %4.1
+% Create a program with if-else statements
+% Identify with if-else statements whether the value in the csv file
+%is positive, negative or zero.
 disp("This is the first exercise: \n");
 dRead = readmatrix('num_value.csv');
 num_val = dRead;
@@ -18,6 +21,7 @@ end
 disp("--------------------------------------------\n");
 %----------------------------------------------------------
 %4.2
+%Identify whether that value belings to a range from 0 to 20 or higher.
 disp("This is the second exercise: \n");
 if (num_val >= 0) & (num_val < 10)
     res_range = '0 <= value < 10'
@@ -31,6 +35,7 @@ end
 disp("--------------------------------------------\n");
 %----------------------------------------------------------
 %4.3
+% certain condition to assess the validity of a statement
 disp("This is the third exercise: \n");
 switch num_val
     case 1
@@ -47,6 +52,7 @@ end
 disp("--------------------------------------------\n");
 %----------------------------------------------------------
 %4.4
+%A switch-case that checkas the first letter of a string
 disp("This is the forth exercise: \n");
 string_var = 'this is a string.'
 switch string_var(1)
--- universityCodes/Ex5/Ex5.m
+++ universityCodes/Ex5/Ex5.m
@@ -5,7 +5,7 @@ clc
 clear
 close
 %5.1
-
+%execute certain operations on the following vectors both by for loops and vectorization
 A = [7 21 30 40];
 B = [11 4 14 6];
 C = [0.5 0.3 0.1 2.7];
@@ -28,7 +28,11 @@ H_vec = A ./ C + B .* B;
 
 
 %5.2
-
+% Create a vector with 200 random numbers in a range
+%between 0 and 100 and name it rand_vec.
+Replace each element with an even index with the value 0.
+%Elements with an odd index have to be
+%doubled. Solve this exercise with a while loop.
 rand_vec = rand(1,200)*100;
 i = 1;
 while(i <= length(rand_vec))
@@ -41,7 +45,10 @@ while(i <= length(rand_vec))
 end
 
 %5.3
-
+%Create a matrix with the dimension 10x10 and
+%fill it with random numbers between 0 and 10 and
+%name this matrix rand_matrix. Multiply each element with
+%the row index, by using for loops
 rand_matrix = rand(10,10)*10;
 for i=1:size(rand_matrix,1)
     for j=1:size(rand_matrix,2)
